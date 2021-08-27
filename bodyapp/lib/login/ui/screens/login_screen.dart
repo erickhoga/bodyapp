ed0dff7cb0d18b4f94f75f3f4584f41d537ee04a
Mario H. Adaniya <mhadaniya@gmail.com>
2021-08-27 19:21:01 -0300

    validaçao de campos de login

--- bodyapp/lib/login/ui/screens/login_screen.dart
+++ bodyapp/lib/login/ui/screens/login_screen.dart
@@ -12,27 +12,109 @@ class LoginScreen extends StatelessWidget {
             color: Colors.orange,
             height: height / 3,
           ),
-          Form(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: InputWidget(
-                    hintText: 'Email',
-                    prefixIcon: Icons.email_outlined,
-                  ),
+          LoginFormWidget(),
+        ],
+      ),
+    );
+  }
+}
+
+class LoginFormWidget extends StatefulWidget {
+  @override
+  _LoginFormWidgetState createState() => _LoginFormWidgetState();
+}
+
+class _LoginFormWidgetState extends State<LoginFormWidget> {
+  RegExp _email = RegExp(r"(\w*\.?\w*@\w*\.(com|net|co|edu))");
+  RegExp _upper = RegExp(r'[A-Z]');
+  RegExp _lower = RegExp(r'[a-z]');
+  RegExp _numeric = RegExp(r'[0-9]');
+
+  bool _isObscure = true;
+  String email = '';
+  String password = '';
+
+  GlobalKey _key = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _key,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: InputWidget(
+              hintText: 'Email',
+              prefixIcon: Icons.email_outlined,
+              onChanged: (value) {
+                setState(() {
+                  email = value;
+                });
+              },
+              validator: (value) {
+                if (value != null && !_email.hasMatch(value)) {
+                  return 'Insira um e-mail válido.';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: InputWidget(
+              hintText: 'Senha',
+              prefixIcon: Icons.lock,
+              sufixIcon: !_isObscure
+                  ? Icons.visibility_outlined
+                  : Icons.visibility_off_outlined,
+              obscureText: _isObscure,
+              suffixIconOnPressed: () {
+                setState(() {
+                  _isObscure = !_isObscure;
+                });
+              },
+              onChanged: (value) {
+                setState(() {
+                  password = value;
+                });
+              },
+              validator: (value) {
+                if (value != null && value.length < 6) {
+                  return 'A senha deve ter mais do que 6 caracteres.';
+                }
+                if (value != null && !value.contains(_upper)) {
+                  return 'A senha deve conter pelo menos um caractere uppercase.';
+                }
+                if (value != null && !value.contains(_lower)) {
+                  return 'A senha deve conter pelo menos um caractere lowercase.';
+                }
+                if (value != null && !value.contains(_numeric)) {
+                  return 'A senha deve conter pelo menos um caractere numerico.';
+                }
+
+                return null;
+              },
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 16,
+              horizontal: 24,
+            ),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: InputWidget(
-                    hintText: 'Senha',
-                    prefixIcon: Icons.lock,
-                    sufixIcon: Icons.visibility,
-                    obscureText: true,
-                    suffixIconOnPressed: () {},
-                  ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Center(
+                  child: Text('Sign In'),
                 ),
-              ],
+              ),
+              onPressed: () {},
             ),
           ),
         ],
@@ -47,6 +129,8 @@ class InputWidget extends StatelessWidget {
   IconData? sufixIcon;
   bool obscureText;
   VoidCallback? suffixIconOnPressed;
+  String? Function(String?)? validator;
+  Function(String)? onChanged;
 
   InputWidget({
     this.hintText,
@@ -54,11 +138,14 @@ class InputWidget extends StatelessWidget {
     this.sufixIcon,
     this.obscureText = false,
     this.suffixIconOnPressed,
+    this.validator,
+    this.onChanged,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      onChanged: onChanged,
       obscureText: obscureText,
       autovalidateMode: AutovalidateMode.onUserInteraction,
       decoration: InputDecoration(
@@ -95,10 +182,7 @@ class InputWidget extends StatelessWidget {
           onPressed: suffixIconOnPressed,
         ),
       ),
-      validator: (value) {
-        if (value != null && value.length > 4) return 'Erro';
-        return null;
-      },
+      validator: validator,
     );
   }
 }
