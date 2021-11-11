13993a2366098e77b7753140bf7305a1e3e0c8a3
Mario H. Adaniya <mhadaniya@gmail.com>
2021-11-11 20:49:26 -0300

    :construction: boilerplate para crud

---
+++ backend/backend/models.py
@@ -0,0 +1,36 @@
+from sqlalchemy import BigInteger, Boolean, Column, DateTime, ForeignKey, \
+    Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(BigInteger, primary_key=True, index=True)
+    email = Column(String, unique=True, unique=True)
+    name = Column(String, nullable=True)
+    hashed_password = Column(String, nullable=True)
+    is_active = Column(Boolean, default=True)
+    birth_date = Column(DateTime, nullable=True)
+
+    measures = relationship("Measure", back_populates="user")
+
+
+class Measure(Base):
+    __tablename__ = "measures"
+
+    id = Column(BigInteger, primary_key=True, index=True)
+    height = Column(Integer, default=0)
+    weight = Column(Integer, default=0)
+    neck = Column(Integer, default=0)
+    chest = Column(Integer, default=0)
+    biceps = Column(Integer, default=0)
+    hips = Column(Integer, default=0)
+    waist = Column(Integer, default=0)
+    thighs = Column(Integer, default=0)
+    calf = Column(Integer, default=0)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    user = relationship("User", back_populates="measures")
---
+++ backend/backend/schemas.py
@@ -0,0 +1,44 @@
+from typing import List, Optional
+from pydantic import BaseModel
+from datetime import datetime
+
+class MeasureBase(BaseModel):
+    height: Optional[int]
+    weight: Optional[int]
+    neck: Optional[int]
+    chest: Optional[int]
+    biceps: Optional[int]
+    hips: Optional[int]
+    waist: Optional[int]
+    thighs: Optional[int]
+    calf: Optional[int]
+    
+
+class MeasureCreate(MeasureBase):
+    pass
+
+
+class Measure(MeasureBase):
+    id: int
+    user_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    birth_date: datetime
+    measures: List[Measure] = []
+
+    class Config:
+        orm_mode = True
