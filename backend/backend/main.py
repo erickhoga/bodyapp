6ba3e78b388120e7491562f84d66a8db1bdd3178
Mario H. Adaniya <mhadaniya@gmail.com>
2021-11-18 21:02:27 -0300

    🔧 adicionado config

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -2,14 +2,30 @@ from typing import List
 
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
+from fastapi_mail import FastMail, MessageSchema,ConnectionConfig
+from pydantic import EmailStr, BaseModel
+
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
+    
+conf = ConnectionConfig(
+    MAIL_USERNAME = "YourUsername",
+    MAIL_PASSWORD = "strong_password",
+    MAIL_FROM = "mario.adaniya@email.com",
+    MAIL_PORT = 1025,
+    MAIL_SERVER = "mailhog",
+    MAIL_TLS = False,
+    MAIL_SSL = False,
+    USE_CREDENTIALS = False,
+    VALIDATE_CERTS = False
+)
 
 app = FastAPI()
 
+    
 def get_db():
     db = SessionLocal()
     try:
@@ -23,7 +39,23 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     if db_user:
         raise HTTPException(status_code=400, detail="E-mail já cadastrado")
 
-    return crud.create_user(db=db, user=user)
+    user = crud.create_user(db=db, user=user)
+    
+    html = """
+    <p>Thanks for using Fastapi-mail</p> 
+    """
+
+    message = MessageSchema(
+        subject="Fastapi-Mail module",
+        recipients=[user.email], 
+        body=html,
+        subtype="html"
+    )
+
+    fm = FastMail(conf)
+    fm.send_message(message)
+    
+    return user
 
 @app.get("/users/", response_model=List[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
